1c27177cd2ce11dd27ecc455b084e22489b1cb79
sbub <svetaibuben@gmail.com>
2020-03-17 19:37:46 +0100

    Adding ErrorProvider

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,20 @@ import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import UserList from "./components/UserList";
 import UserDetails from "./components/UserDetails";
 import ErrorBoundary from "./components/ErrorBoundary";
+import ErrorProvider from "./context/ErrorProvider";
 
 const App = () => {
   return (
     <ErrorBoundary>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={UserList} />
-          <Route path="/user/:username" component={UserDetails} />
-          <Route path="*" render={() => <Redirect to="/" />} />
-        </Switch>
-      </BrowserRouter>
+      <ErrorProvider>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={UserList} />
+            <Route path="/user/:username" component={UserDetails} />
+            <Route path="*" render={() => <Redirect to="/" />} />
+          </Switch>
+        </BrowserRouter>
+      </ErrorProvider>
     </ErrorBoundary>
   );
 };
